3fb4df11b9442addf52303002af6efbeeb363463
cavis <ryancavis@gmail.com>
2024-05-02 15:50:42 -0600

    Init athena querying

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 const log = require("lambda-log");
+
+const athena = require("./lib/athena");
 const bigquery = require("./lib/bigquery");
 const util = require("./lib/util");
-
-/**
- * Rollup a single day
- */
-async function rollup(day) {
-  const dayStr = day.toISOString().split("T").shift();
-  log.info(`rolling up ${dayStr}`);
-}
 
 /**
  * Entrypoint
  */
 exports.handler = async (event = {}) => {
   if (event.day) {
-    await rollup(event.day);
+    await rollup(util.parseDate(event.day));
   } else {
     const start = await bigquery.latestRollupDay();
     const days = util.daysToProcess(start);
@@ -24,3 +18,17 @@ exports.handler = async (event = {}) => {
     }
   }
 };
+
+/**
+ * Rollup a single day
+ */
+async function rollup(day) {
+  const dayStr = day.toISOString().split("T").shift();
+  log.info(`rolling up ${dayStr}`);
+
+  const [elapsed, rows] = await util.elapsed(() => athena.queryUsage(day));
+  const parsed = util.parseUsage(day, rows);
+  const total = parsed.reduce((sum, row) => sum + row.bytes, 0);
+  const info = { rows: rows.length, parsed: parsed.length, total: total };
+  log.info(`athena ${dayStr}`, { elapsed, ...info });
+}
