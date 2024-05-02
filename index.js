db0f36153257f4be9869474fa468360170dddb8e
cavis <ryancavis@gmail.com>
2024-05-02 14:45:01 -0600

    Start on bigquery

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-export const handler = async (event, context) => {
-  console.log("hello world", event, context);
+const log = require("lambda-log");
+const bigquery = require("./lib/bigquery");
+const util = require("./lib/util");
+
+/**
+ * Rollup a single day
+ */
+async function rollup(day) {
+  const dayStr = day.toISOString().split("T").shift();
+  log.info(`rolling up ${dayStr}`);
+}
+
+/**
+ * Entrypoint
+ */
+exports.handler = async (event = {}) => {
+  if (event.day) {
+    await rollup(event.day);
+  } else {
+    const start = await bigquery.latestRollupDay();
+    const days = util.daysToProcess(start);
+    for (const day of days) {
+      await rollup(day);
+    }
+  }
 };
